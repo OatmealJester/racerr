1a7c6257ae3476b7acf88321ad272d0d9e4471c1
Luiscanessa <117101191+Luiscanessa@users.noreply.github.com>
2025-10-25 02:09:12 -0400

    got the getters and setters for track object

--- models/Track.ts
+++ models/Track.ts
@@ -1,11 +1,59 @@
 export class Track {
+    private _creator: string;
+    private _UID: string;
+    private _best_time: number;
+    private _best_racer: string;
+    private _track_name: string;
+
     constructor(
-        public creator: string,
-        public UID: string,
-        public best_time: number,
-        public best_racer: string,
-        public track_name: string
-    ) { }
+        creator: string,
+        UID: string,
+        best_time: number,
+        best_racer: string,
+        track_name: string
+    ) {
+        this._creator = creator;
+        this._UID = UID;
+        this._best_time = best_time;
+        this._best_racer = best_racer;
+        this._track_name = track_name;
+    }
+
+    public get creator(): string {
+        return this._creator;
+    }
+    public set creator(value: string) {
+        this._creator = value;
+    }
+
+    public get UID(): string {
+        return this._UID;
+    }
+    public set UID(value: string) {
+        this._UID = value;
+    }
+
+    public get best_time(): number {
+        return this._best_time;
+    }
+    public set best_time(value: number) {
+        if (value < 0) throw new Error("Best time cannot be negative.");
+        this._best_time = value;
+    }
+
+    public get best_racer(): string {
+        return this._best_racer;
+    }
+    public set best_racer(value: string) {
+        this._best_racer = value;
+    }
+
+    public get track_name(): string {
+        return this._track_name;
+    }
+    public set track_name(value: string) {
+        this._track_name = value;
+    }
 }
 
 export class TrackBuilder {
