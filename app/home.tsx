6e799aad94858f66e22b4d128da1820ed68a5d7f
Clay Moerler <74159961+ClayMoerler@users.noreply.github.com>
2025-10-25 19:32:39 -0400

    boom shakalaka

--- app/home.tsx
+++ app/home.tsx
@@ -1,64 +1,67 @@
 import { useRouter } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-import HomeIcon from '../assets/images/home.svg';
-import MapIcon from '../assets/images/map.svg';
-import StarIcon from '../assets/images/star.svg';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeIcon from '../../assets/images/home.svg';
+import MapIcon from '../../assets/images/map.svg';
+import StarIcon from '../../assets/images/star.svg';
 import IconButton from '../components/IconButton';
+
+
 
 export default function home() {
   
   const router = useRouter();
   
   return (
-          <View style={styles.mainWrapper}>
-        
-          <View style={styles.taskBar}>
-            {/* Icon 1: Home */}
-            <IconButton 
-              IconComponent={HomeIcon}  
-              size={40}                 
-              onPress={() => router.push('/home')} 
-            />
-          
-          {/* Icon 3: Map */}
-          <IconButton 
-            IconComponent={MapIcon}
-            size={40}
-            onPress={() => router.push('/mapScreen')}
-          />
-
-          {/* Icon 3: Star */}
-          <IconButton 
-            IconComponent={StarIcon}
-            size={40}
-            onPress={() => router.push('/favoriteScreen')}
-          />
-          
-        </View>
+    
+    /* Main Page Content */
+    <View style={styles.mainWrapper}>
+      <View style={styles.container}>
+        <Text style={styles.text}> YAy </Text>
       </View>
+    
+    {/* Taskbar Content */}
+    <View style={styles.taskBar}>
+
+      {/* Icon 1: Home */}
+        <IconButton 
+          IconComponent={HomeIcon}  
+          size={40}                 
+          onPress={() => router.push('./home')} 
+        />
+          
+      {/* Icon 3: Map */}
+        <IconButton 
+          IconComponent={MapIcon}
+          size={40}
+          onPress={() => router.push('./mapScreen')}
+        />
+
+      {/* Icon 3: Star */}
+        <IconButton 
+          IconComponent={StarIcon}
+          size={40}
+          onPress={() => router.push('./favoriteScreen')}
+        />
+      </View>    
+    </View>
     );
   }
 
-const modalStyles = StyleSheet.create({
-  closeButtonContainer: {
-    position: 'absolute',
-    top: 60,
-    left: 20,
-    zIndex: 1,
-  },
-});
 const styles = StyleSheet.create({
   mainWrapper: {
     flex:1,
     backgroundColor: '#25292e',
+    flexDirection: 'column',
   },
   taskBar: {
-    paddingVertical: 10,
-    paddingHorizontal: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    backgroundColor: '#333',
+    backgroundColor: "#E02a24",
+    alignItems: "center",
+    justifyContent: "space-around",
   },
+
 
   container: {
     flex: 1,
@@ -68,6 +71,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+    fontSize: 10,
   },
   image: {
     width: 80,
